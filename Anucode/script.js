e742f719b047b8850a8087b3fc52bc4f98861f9d
Anupama Dhir <sabhssabhs@yahoo.com>
2016-08-09 09:56:47 +0800

    Anu's files

--- Anucode/script.js
+++ Anucode/script.js
@@ -1,25 +1,34 @@
-$(document).ready(function() {
+$(function() {
 
-  'use strict';
+    'use strict';
 
-  $(".text_process").click(function(){
-
-      $.ajax({
-          url: 'https://yoda.p.mashape.com/yoda?sentence=', // The URL to the API. You can get this by clicking on "Show CURL example" from an API profile
-          headers: {'X-Mashape-Key': 'E6ybhccsTZmshUkwhkwrkaeknizDp1fVtxzjsnzmLq3sBvNcM5'},
-          type: 'GET', // The HTTP Method
-          data: {sentence: $("#yoda_input").val()}, // Additional parameters here
-          datatype: 'json',
-          success: function (data) {
-              $("#output").html(data);
-          },
-          error: function (err) {
-              alert(err);
-          },
-          beforeSend: function (xhr) {
-              xhr.setRequestHeader("X-Mashape-Authorization", "E6ybhccsTZmshUkwhkwrkaeknizDp1fVtxzjsnzmLq3sBvNcM5"); // Enter here your Mashape key
-          }
+    $(".text_process").click(function() {
+        $.ajax({
+            // where the data live
+            url: 'https://yoda.p.mashape.com/yoda',
+            headers: {
+              'X-Mashape-Key': 'E6ybhccsTZmshUkwhkwrkaeknizDp1fVtxzjsnzmLq3sBvNcM5',
+              'Accept': 'text/plain'
+            },
+            type: 'GET',
+            data: {sentence: $("#yoda_input").val()},
+            dataType: 'html',
+            beforeSend: function(xhr) {
+              xhr.setRequestHeader("X-Mashape-Authorization", "E6ybhccsTZmshUkwhkwrkaeknizDp1fVtxzjsnzmLq3sBvNcM5");
+            }
+          }).done(successFunction)
+            .fail(failFunction);
       });
 
-  });
-});
+
+      //success function
+      function successFunction(data, dataType, status) {
+        $("#output").text(data);
+      };
+
+
+      // fail function
+      function failFunction(request, textStatus, errorThrown) {
+          alert('An error occurred during your request: ' + request.status + ' ' + textStatus + ' ' + errorThrown);
+      };
+    });
